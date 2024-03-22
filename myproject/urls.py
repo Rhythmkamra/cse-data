146c389e584eb611266b0dd1f63cd69d8393451d
rhythm <itsmerhythmk@gmail.com>
2024-03-22 13:04:34 +0530

    added forms.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import data
+from data import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
 ]
