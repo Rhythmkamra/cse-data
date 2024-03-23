eb398d0556f874fcc852606d3f43e9941589c843
rhythm <itsmerhythmk@gmail.com>
2024-03-23 08:54:36 +0530

    updated the models

--- data/forms.py
+++ data/forms.py
@@ -1,32 +1,37 @@
 from django import forms
-from .models import Student_cultural,Student_sports,Student_technical
+from .models import Student_cultural, Student_sports, Student_technical
 
 class StudentFormCultural(forms.ModelForm):
     class Meta:
         model = Student_cultural
-        fields = ['name', 'rollno', 'year', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
+        fields = ['name', 'rollno', 'year', 'event_name', 'event_description', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
 
     def __init__(self, *args, **kwargs):
         super(StudentFormCultural, self).__init__(*args, **kwargs)
         self.fields['event_type'].initial = Student_cultural.EVENT_TYPE_DEFAULT
+        self.fields['name'].initial = 'Default Name'
+        self.fields['event_description'].initial = 'Default Description'
 
 
 class StudentFormSports(forms.ModelForm):
     class Meta:
         model = Student_sports
-        fields = ['name', 'rollno', 'year', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
+        fields = ['name', 'rollno', 'year', 'event_name', 'event_description', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
 
     def __init__(self, *args, **kwargs):
         super(StudentFormSports, self).__init__(*args, **kwargs)
         self.fields['event_type'].initial = Student_sports.EVENT_TYPE_DEFAULT
-
+        self.fields['name'].initial = 'Default Name'
+        self.fields['event_description'].initial = 'Default Description'
 
 
 class StudentFormTechnical(forms.ModelForm):
     class Meta:
         model = Student_technical
-        fields = ['name', 'rollno', 'year', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
+        fields = ['name', 'rollno', 'year', 'event_name', 'event_description', 'event_type', 'intercollege_mcq', 'intracollege_mcq', 'position', 'is_active']
 
     def __init__(self, *args, **kwargs):
         super(StudentFormTechnical, self).__init__(*args, **kwargs)
         self.fields['event_type'].initial = Student_technical.EVENT_TYPE_DEFAULT
+        self.fields['name'].initial = 'Default Name'
+        self.fields['event_description'].initial = 'Default Description'
--- data/models.py
+++ data/models.py
@@ -22,15 +22,20 @@ class Student_cultural(models.Model):
     )
     
     EVENT_TYPE_DEFAULT = 'cultural'  # Default value for event_type
+    EVENT_NAME_DEFAULT = 'Cultural Event'  # Default value for event_name
+    EVENT_DESC_DEFAULT = 'This is a cultural event.'  # Default value for event_description
 
-    name = models.CharField(max_length=100)
-    rollno = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for name
+    rollno = models.CharField(max_length=20, default='')  # Example default value for rollno (you can change it)
     year = models.IntegerField(choices=YEAR_CHOICES)
+    event_name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for event_name
+    event_description = models.TextField(default=EVENT_DESC_DEFAULT)  # Added default value for event_description
     event_type = models.CharField(max_length=20, choices=EVENT_TYPE_CHOICES, default=EVENT_TYPE_DEFAULT)
     intercollege_mcq = models.BooleanField(default=False)
     intracollege_mcq = models.BooleanField(default=False)
     position = models.CharField(max_length=10, choices=POSITION_CHOICES)
     is_active = models.BooleanField(default=True)  # Default field
+    
     def __str__(self):
         return self.name
     
@@ -59,15 +64,20 @@ class Student_sports(models.Model):
     )
     
     EVENT_TYPE_DEFAULT = 'cultural'  # Default value for event_type
+    EVENT_NAME_DEFAULT = 'Sports Event'  # Default value for event_name
+    EVENT_DESC_DEFAULT = 'This is a sports event.'  # Default value for event_description
 
-    name = models.CharField(max_length=100)
-    rollno = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for name
+    rollno = models.CharField(max_length=20, default='')  # Example default value for rollno (you can change it)
     year = models.IntegerField(choices=YEAR_CHOICES)
+    event_name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for event_name
+    event_description = models.TextField(default=EVENT_DESC_DEFAULT)  # Added default value for event_description
     event_type = models.CharField(max_length=20, choices=EVENT_TYPE_CHOICES, default=EVENT_TYPE_DEFAULT)
     intercollege_mcq = models.BooleanField(default=False)
     intracollege_mcq = models.BooleanField(default=False)
     position = models.CharField(max_length=10, choices=POSITION_CHOICES)
     is_active = models.BooleanField(default=True)  # Default field
+    
     def __str__(self):
         return self.name    
     
@@ -95,14 +105,19 @@ class Student_technical(models.Model):
     )
     
     EVENT_TYPE_DEFAULT = 'cultural'  # Default value for event_type
+    EVENT_NAME_DEFAULT = 'Technical Event'  # Default value for event_name
+    EVENT_DESC_DEFAULT = 'This is a technical event.'  # Default value for event_description
 
-    name = models.CharField(max_length=100)
-    rollno = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for name
+    rollno = models.CharField(max_length=20, default='')  # Example default value for rollno (you can change it)
     year = models.IntegerField(choices=YEAR_CHOICES)
+    event_name = models.CharField(max_length=100, default=EVENT_NAME_DEFAULT)  # Added default value for event_name
+    event_description = models.TextField(default=EVENT_DESC_DEFAULT)  # Added default value for event_description
     event_type = models.CharField(max_length=20, choices=EVENT_TYPE_CHOICES, default=EVENT_TYPE_DEFAULT)
     intercollege_mcq = models.BooleanField(default=False)
     intracollege_mcq = models.BooleanField(default=False)
     position = models.CharField(max_length=10, choices=POSITION_CHOICES)
     is_active = models.BooleanField(default=True)  # Default field
+    
     def __str__(self):
         return self.name    
